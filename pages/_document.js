63382cc290bb071a1f494e1fb2f41aa7485e263b
Abdur Rofi <rofim58@gmail.com>
2024-05-05 20:13:40 +0700

    update

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,25 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import config from '@/data/config'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import config from "@/data/config";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en" className="scroll-smooth focus:scroll-auto">
         <Head>
-            <link rel="icon" href={config.favIcon} type="image/png" />
+          <link rel="icon" href={config.favIcon} type="image/png" />
         </Head>
         <body className="bg-white dark:bg-gray-800 transition-colors duration-100 ease-linear">
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
